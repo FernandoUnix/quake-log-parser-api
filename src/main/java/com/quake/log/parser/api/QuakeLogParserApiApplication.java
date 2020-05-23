323491467e7c62c0df14742a3a28d68b605d8437
Fernando Jose <openline_desenvolvimento2@outlook.com>
2020-05-23 14:37:26 -0300

    inicio projeto

---
+++ src/main/java/com/quake/log/parser/api/QuakeLogParserApiApplication.java
@@ -0,0 +1,13 @@
+package com.quake.log.parser.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuakeLogParserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuakeLogParserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quake/log/parser/api/QuakeLogParserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quake.log.parser.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuakeLogParserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
