1c0677c05115bbb6733bf513f9573399cc770595
Fernando Jose <openline_desenvolvimento2@outlook.com>
2020-05-23 22:59:50 -0300

    adicionado swagger para documentação da api

--- src/main/java/com/quake/log/parser/api/resource/QuakeParserResource.java
+++ src/main/java/com/quake/log/parser/api/resource/QuakeParserResource.java
@@ -9,21 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.quake.log.parser.api.service.QuakeParserToMapService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(value = "quake parser")
 public class QuakeParserResource {
 
 	@Autowired
 	private QuakeParserToMapService quakeParserToMapService;
-	
+
 	@GetMapping("/partidas/{id}")
-	public ResponseEntity<?> getPartida(@PathVariable Long id){
+	@ApiOperation(value = "Obter partida por Id")
+	public ResponseEntity<?> getPartida(@PathVariable Long id) {
 		Map<String, Object> partida = this.quakeParserToMapService.getPartida(id);
 		return new ResponseEntity<Map<String, Object>>(partida, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/partidas")
+	@ApiOperation(value = "Obter todas as partidas")
 	public ResponseEntity<?> partidas() {
 
 		Map<String, Object> partida = this.quakeParserToMapService.getPartidas();
